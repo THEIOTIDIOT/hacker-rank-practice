95a7c27d7435ebb50b3eeb6c14ae7b81fe2eeaaa
BENZ <benzimmer.is@gmail.com>
2021-10-21 21:24:50 -0500

    challenge1-1

--- test-prep/challenge1.py
+++ test-prep/challenge1.py
@@ -13,8 +13,28 @@ import sys
 #
 
 def plusMinus(arr):
-    # Write your code here
-    re.findall(())
+    # Write your code here 
+    count = len(arr)
+    neg = float(0)
+    zero = float(0)
+    pos = float(0)
+    
+    for num in arr:
+        if num == 0:
+            zero += 1
+        elif num < 0:
+            neg += 1
+        elif num > 0:
+            pos += 1
+    if count == 0:
+        print("{:.6f}".format(float(0)))
+        print("{:.6f}".format(float(0)))
+        print("{:.6f}".format(float(0)))
+    else:
+        print("{:.6f}".format(pos/count))
+        print("{:.6f}".format(neg/count))
+        print("{:.6f}".format(zero/count)) 
+
 if __name__ == '__main__':
     n = int(input().strip())
 
