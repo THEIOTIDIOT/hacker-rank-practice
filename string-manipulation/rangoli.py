82c85bf4f21f60a3265276177aedcb580454f4ad
BENZ <benzimmer.is@gmail.com>
2021-12-14 20:11:55 -0600

    RANGOLI!!

--- string-manipulation/rangoli.py
+++ string-manipulation/rangoli.py
@@ -12,14 +12,19 @@ def print_rangoli(size):
 
     lines = []
     for i in range(size-1,0,-1):
-        lines.append("-".join(subset[::-1][:size-i]+subset[::-1][:size-i-1]).center(size*3,"-"))
+        lines.append("-".join(subset[::-1][:size-i]+
+                                subset[i+1:len(subset)]).center(((size + (size-1))*2)-1,"-"))
 
     for i in range(0,size):
-        lines.append("-".join(subset[::-1][:size-i]+subset[::-1][:size-i-1]).center(size*3,"-"))
+        lines.append("-".join(subset[::-1][:size-i]+
+                                subset[1+i:]).center(((size + (size-1))*2)-1,"-"))
     
     print("\n".join(lines))
 
     
 if __name__ == '__main__':
     #n = int(input())
+    print_rangoli(5)
     print_rangoli(3)
+    print_rangoli(7)
+    print_rangoli(8)
