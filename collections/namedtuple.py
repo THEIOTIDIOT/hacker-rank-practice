1fc480f82f030f7b44f7c7afa7228ff0ee118362
BENZ <benzimmer.is@gmail.com>
2022-01-17 12:53:29 -0600

    parse data from unordered tab delimited csv with named tuple

--- collections/namedtuple.py
+++ collections/namedtuple.py
@@ -1,3 +1,10 @@
+"""
+#the best way to do this if you are getting input from STDIN
+from collections import namedtuple
+nos = int(input())
+StudentData = namedtuple('StudentData', ', '.join(input().split()))
+print(sum(map(float,[student.MARKS for student in [StudentData(*input().split()) for _ in range(nos)]]))/nos)"""
+
 testcase1 = """5
 ID         MARKS      NAME       CLASS     
 1          97         Raymond    7         
@@ -12,3 +19,4 @@ from collections import namedtuple
 StudentData = namedtuple('StudentData', 'id,mark,name,course')
 nos = int(tc[0])
 print(sum(map(float,[student.mark for student in [StudentData(*tc[i].split()) for i in range(2,nos + 2)]]))/nos)
+
