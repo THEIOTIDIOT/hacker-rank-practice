cea0178a075f13ca21c8bed4a69378eb547d80c6
BENZ <benzimmer.is@gmail.com>
2022-01-27 22:22:46 -0600

    sets and stuff

---
+++ sets/difference.py
@@ -0,0 +1,4 @@
+
+# difference will subtract anything in the first set that is also in the second set
+s = set([1,2,3,4,5,6,7,8,9])
+print(len(s.difference(set([10,1,2,3,11,21,55,6,8]))))
---
+++ sets/intersection.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# intesection returns numbers that are in both sets
+s = set([1,2,3,4,5,6,7,8,9])
+print(len(s.intersection(set([10,1,2,3,11,21,55,6,8]))))
