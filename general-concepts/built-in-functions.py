96bb79fb8d0f7aa5c641f9abeed492747f30c531
BENZ <benzimmer.is@gmail.com>
2022-01-30 10:35:03 -0600

    new

---
+++ general-concepts/built-in-functions.py
@@ -0,0 +1,22 @@
+# List of built in functions
+####### sum()
+# argument has to be an iterable
+print(sum(set([1,2,3,4,5])))
+
+####### divmod()
+# returns set with integer div (a//b) and remainder
+# One of the built-in functions of Python is divmod, 
+# which takes two arguments  and  and returns a tuple 
+# containing the quotient of  first and then the remainder .
+
+# >>> print divmod(177,10)
+# (17, 7)
+print(divmod(177, 10))
+
+####### pow() or a**b
+# It's also possible to calculate a**b mod m (pow(a,b,m))
+# modulo can't be negative
+print(pow(17,10)) 
+print(pow(17,10,3)) # returns remainder when divided by modulo
+
+
